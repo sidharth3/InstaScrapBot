b3317b2d4d67f7230f7c0bfa6da6f41209d5442d
sidharth3 <sidhu.beena@gmail.com>
2019-07-22 20:15:04 +0800

    finished signin method

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ class InstaBot():
         self.browser = webdriver.Chrome()
 
     def signIn(self):
-        self.browser.get('https://www.instagram.com/accounts/login/)
+        self.browser.get('https://www.instagram.com/accounts/login/')
         uName = self.browser.find_element_by_name("username")
-        
+        passW = self.browser.find_element_by_name("password")
+        uName.send_keys(self.username)
+        passW.send_keys(self.password)
+        passW.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+    
+bot = InstaBot('', '') #Enter your username and password here
+bot.signIn()
+
+
